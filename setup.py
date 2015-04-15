ebb1d6bffe6804fbf18419ce26a36dfcd3cdd71e
Daniel Moran <danielheth@hotmail.com>
2015-04-15 12:46:08 -0500

    fixing installer bug

--- setup.py
+++ setup.py
@@ -15,19 +15,19 @@ setup(
 	name='hethio-server',
 	version=read('VERSION.txt'),
 	install_requires = [], #['paho-mqtt','mysql-connector-python'],  # python dependencies
-	setup_requires = [],
+	
 	packages = find_packages(),
 	py_modules = ['hethio-server'],
 	scripts=[
 		'usr-bin/hethio-server'
 		],
 	data_files = [
-		('share/hethio-server/', glob.glob('hethio_data/*')),
-		('/etc/init.d/', glob.glob('etc-init.d/*')),
-		('/usr/share/man/man8/', glob.glob('usr-share-man-man8/*.gz'))
+		('share/hethio-server', glob.glob('hethio_data/*')),
+		('/etc/init.d', glob.glob('etc-init.d/*')),
+		('/usr/share/man/man8', glob.glob('usr-share-man-man8/*.gz'))
 		],
 	package_data = {  # Files moved into /usr/share/hethio/
-        'hethio_data': ['*.png']
+        'hethio_data': ['*.png'],
     	},
     include_package_data = True, 
     zip_safe = True,
